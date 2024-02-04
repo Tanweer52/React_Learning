133fd83e16c67ed43197be6a02cf35f9fe58637c
Tanweer Ansari <86672072+Tanweer52@users.noreply.github.com>
2024-02-04 17:38:39 +0530

    Ep1

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement(
+    "h2",
+    { id: "heading" },
+["Hello World from React"]
+    );
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
